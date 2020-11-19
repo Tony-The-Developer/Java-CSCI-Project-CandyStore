36fcc8b1908d5320d2bc04496a91e5453480c150
Tony Ashvanian <tony@tonythedeveloper.com>
2020-11-19 09:31:51 -0800

    add code

---
+++ src/edu/glendale/csci112/TonyCandyINC.java
@@ -0,0 +1,17 @@
+package edu.glendale.csci112;
+
+public class TonyCandyINC {
+    public static void main (String [] arg) {
+//        Snickers
+        int snickers = 20;
+        int snickersprice = 90;
+//        Lolly Pops
+        int lollypops = 60;
+        int lollypopprice = 20;
+
+        System.out.println("Current Products Inventory:");
+        System.out.println("Snickers:" + snickers + " They are " + snickersprice + " cents each");
+        System.out.println("Lolly Pops:" + lollypops+ " They are " + lollypopprice + " cents each");
+
+    }
+}
